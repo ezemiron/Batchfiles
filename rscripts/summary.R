0d57a29d4316aeb0c9ebd0b858106d0180b7a22f
Ezequiel Miron <eze.miron@bioch.ox.ac.uk>
2015-10-13 11:24:42 +0100

    this is the file that takes all the output data for individual experiments of a specific biological target and groups them together in one file. It's not necessary but convenient.

---
+++ rscripts/summary.R
@@ -0,0 +1,33 @@
+#library(bioimagetools)
+#library(parallel)
+#library(tcltk)
+require(bioimagetools)
+require(parallel)
+require(tcltk)
+require(EBImage)
+
+#Choose a directory:
+dirchosen <- tk_choose.dir(caption = "Select directory");
+setwd(dirchosen)
+
+varname  <- readline(prompt = "Enter immunofluorescense target:")
+# name  <- paste0(varname, "*-distn.csv")
+# n <- c(NA,NA,NA,NA,NA,NA,NA,NA)
+# df  <- data.frame(1:7)
+# df[1] <- NULL
+df  <- data.frame(class=1:7)
+df# generates a list of files in that directory:
+allfilesandpaths <- list.files(path=dirchosen, pattern= varname, full.names=TRUE);
+filesandpaths  <- grep("*-distn.csv",allfilesandpaths, value = TRUE)
+for (fileandpath in filesandpaths){
+  
+  csv1 <- read.csv(fileandpath, colClasses=c("NULL","NULL","NULL","NULL","NULL","NULL", NA,"NULL"))
+  df  <- cbind(df,csv1)
+
+  
+}
+df <- transform(df, AvNorm = rowMeans(df[,-1], na.rm = TRUE))
+df <- transform(df, Log2AvNorm = log2(df$AvNorm))
+
+savename  <- paste0(varname,"_distn-summary.csv")
+write.csv(df,savename);
