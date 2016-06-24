735e56a230b85599d7de0e699dea540736d1c3e3
Eze Miron <eze.miron@bioch.ox.ac.uk>
2016-06-24 18:37:31 +0100

    rscripts/histograms_d2b.R: iron out discrepancies. Still need to make this file run in batch.

--- rscripts/histograms_d2b.R
+++ rscripts/histograms_d2b.R
@@ -1,14 +1,14 @@
 
 #steps for plotting results of d2b on same graph using ggplot2:
 
-library (ggplot)
+library (ggplot2)
 
 G1 <- read.csv("path_to_G1")
 G0 <- read.csv("path_to_G0")
 
 #take only the distance:
-G1 <- G1$x
-G0 <- G0$x
+G1 <- as.data.frame(G1$x)
+G0 <- as.data.frame(G0$x)
 
 #give phase ID:
 G1$phase <- "G1"
@@ -18,11 +18,11 @@ G0$phase <- "G0"
 names(G1) <- rep(NA, ncol(G1))
 names(G0) <- rep(NA, ncol(G0))
 comp <- rbind(G1,G0)
-names(comp) <- c("microns", "phase")
+names(comp) <- c("Microns", "Phase")
 
 #different ways of plotting:
 #density graph:
-ggplot(comp1, aes(microns, fill = phase)) + geom_density(alpha = 0.2)
+ggplot(comp, aes(Microns, fill = Phase)) + geom_density(alpha = 0.2)
 #histogram:
-ggplot(comp1, aes(microns, fill = phase)) + geom_histogram(alpha = 0.5)
+ggplot(comp, aes(Microns, fill = Phase)) + geom_histogram(alpha = 0.5)
 
